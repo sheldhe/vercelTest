2a40461f084cdfe5fcbe599f8b25249184b3f5a6
Daehan Choi <sheldhe93@gmail.com>
2024-10-14 16:21:26 +0900

    - getMovie export

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 
 export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export async function getMovies() {
+async function getMovies() {
   const response = await fetch(API_URL);
   const json = await response.json();
   return json;
